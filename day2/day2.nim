e73ea9ba0cfb98b366463d754f904ac3ae8000e2
Simo-Pekka Kerkelä <simo-pekka.kerkela@reaktor.com>
2020-12-03 17:30:34 +0200

    day 2 part 2

--- day2/day2.nim
+++ day2/day2.nim
@@ -4,12 +4,33 @@ type PasswordRequirement* = object
     min*: int
     max*: int
 
+type TruePasswordRequirement* = object
+    c*: char
+    first*: int
+    second*: int
+
 proc isValidPassword*(password: string, requirement: PasswordRequirement) : bool =
     var count = 0
     for ch in password:
         if ch == requirement.c:
             count += 1
     result = count >= requirement.min and count <= requirement.max
+
+proc isValidTruePassword*(password: string, requirement: TruePasswordRequirement) : bool =
+    let
+        firstIsOk = password[requirement.first-1] == requirement.c
+        secondIsOk = password[requirement.second-1] == requirement.c
+    result = firstIsOk xor secondIsOk
+
+proc parseTruePasswordRequirement*(str : string) : TruePasswordRequirement =
+    let
+        ruleParts = str.splitWhitespace()
+        c = ruleParts[1].strip()[0]
+        firstAndSecond = ruleParts[0].split('-')
+        first = parseInt(firstAndSecond[0])
+        second = parseInt(firstAndSecond[1])
+        passwordRequirement = TruePasswordRequirement(c:c, first:first, second:second)
+    result = passwordRequirement
 
 proc parsePasswordRequirement*(str : string) : PasswordRequirement =
     let
@@ -21,19 +42,27 @@ proc parsePasswordRequirement*(str : string) : PasswordRequirement =
         passwordRequirement = PasswordRequirement(c:c, min:min, max:max)
     result = passwordRequirement
 
-proc handleLine*(l : string) : bool =
+proc handleLine*(l : string, useCorrect:bool=true) : bool =
     let
         parts = split(l, ':')
         rule = parts[0]
-        passwordRequirement = parsePasswordRequirement(rule)
         password = parts[1].strip()
-    result = isValidPassword(password, passwordRequirement)
+    if useCorrect:
+        result = isValidTruePassword(password, parseTruePasswordRequirement(rule))
+    else:
+        result = isValidPassword(password, parsePasswordRequirement(rule))
 
 proc run*(fileName: string) =
     let file = readFile(fileName)
     let lines = splitLines(file)
     var valid = 0
     for line in lines:
-        if handleLine(line):
+        if handleLine(line, false):
             valid += 1
     echo valid
+    valid = 0
+    for line in lines:
+        if handleLine(line, true):
+            valid += 1
+    echo valid
+
--- day2/tests.nim
+++ day2/tests.nim
@@ -14,12 +14,19 @@ suite "day 2 suite":
     test "Lists are equal":
         check([1,2,3] == [1,2,3])
     
-    test "Handle line":
+    test "ParseRequirement":
         check(PasswordRequirement(c:'a', min:1, max:3) == parsePasswordRequirement("1-3 a"))
+        check(TruePasswordRequirement(c:'a', first:1, second:3) == parseTruePasswordRequirement("1-3 a"))
         check(PasswordRequirement(c:'b', min:1, max:3) == parsePasswordRequirement("1-3 b"))
+        check(TruePasswordRequirement(c:'b', first:1, second:3) == parseTruePasswordRequirement("1-3 b"))
         check(PasswordRequirement(c:'c', min:2, max:9) == parsePasswordRequirement("2-9 c"))
+        check(TruePasswordRequirement(c:'c', first:2, second:9) == parseTruePasswordRequirement("2-9 c"))
     
     test "Check password":
         check(isValidPassword("abcde", PasswordRequirement(c:'a', min:1, max:3)))
         check(not isValidPassword("cdefg", PasswordRequirement(c:'b', min:1, max:3)))
         check(isValidPassword("ccccccccc", PasswordRequirement(c:'c', min:2, max:9)))
+
+        check(isValidTruePassword("abcde", TruePasswordRequirement(c:'a', first:1, second:3)))
+        check(not isValidTruePassword("cdefg", TruePasswordRequirement(c:'b', first:1, second:3)))
+        check(not isValidTruePassword("ccccccccc", TruePasswordRequirement(c:'c', first:2, second:9)))
